636abb6e843abe16e4befdaed44074dd46f5bca8
Nicolas Fricke <mail@nicolasfricke.com>
2022-05-04 17:08:00 +0200

    Print the picture we just took
    Change main.py to first take a picture and then print it.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import sys
+from camera import take_picture
 from image import load_resize_and_save
 from printer import print_image
 
 
 if __name__ == '__main__':
-    if (len(sys.argv) != 2):
-        print("Please provide the path to the image to print")
-        sys.exit(1)
-
-    image_path = sys.argv[1]
+    print("Taking a picture...")
+    image_path = take_picture()
+    print("Done, saved as " + image_path)
 
     print("Resizing " + image_path)
     resized_image_path = load_resize_and_save(image_path)
