a7d235c0f9d1c7bb28405c0b9e4b50eef1957da2
Nicolas Fricke <mail@nicolasfricke.com>
2022-05-04 14:54:18 +0200

    Reformat code to make it more python-esque
    This will allow us to also include the files in others without
    triggering their scripts when including.

--- main.py
+++ main.py
@@ -3,34 +3,40 @@ import os
 import time
 from escpos.printer import Serial, Dummy
 
-if (len(sys.argv) != 2):
-    print("Please provide the path to the image to print")
-    sys.exit(1)
 
-image_path = sys.argv[1]
-print("Printing " + image_path)
+def print_image(image_path):
+    p = Serial(devfile='/dev/cu.usbserial-0001',
+               baudrate=9600,
+               bytesize=8,
+               parity='N',
+               stopbits=1,
+               timeout=0.05,
+               dsrdtr=False)
 
-p = Serial(devfile='/dev/cu.usbserial-0001',
-           baudrate=9600,
-           bytesize=8,
-           parity='N',
-           stopbits=1,
-           timeout=0.05,
-           dsrdtr=False)
+    d = Dummy()
 
-d = Dummy()
+    d.text("This is my image " + image_path + ":\n")
+    d.image(image_path, impl="bitImageColumn")
+    d.print_and_feed(5)  # Push the paper out
 
-d.text("This is my image " + image_path + ":\n")
-d.image(image_path, impl="bitImageColumn")
-d.print_and_feed(5) # Push the paper out
+    os.makedirs("./tmp", exist_ok=True)
+    with open("./tmp/output", "wb") as f:
+        f.write(d.output)
+        print("Dumped output to ./tmp/output")
 
-os.makedirs("./tmp", exist_ok=True)
-with open("./tmp/output", "wb") as f:
-    f.write(d.output)
-    print("Dumped output to ./tmp/output")
+    print("Printing...")
 
-print("Printing...")
+    p._raw(d.output)
+    time.sleep(10)  # To ensure the print job is done before quitting
+    p.close()
 
-p._raw(d.output)
 
-time.sleep(10) # To ensure the print job is done
+if __name__ == '__main__':
+    if (len(sys.argv) != 2):
+        print("Please provide the path to the image to print")
+        sys.exit(1)
+
+    image_path = sys.argv[1]
+    print("Printing " + image_path)
+
+    print_image(image_path)
