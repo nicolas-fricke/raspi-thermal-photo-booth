41f9162aa7c6c93bd53739a11de712a7d2edf6d1
Nicolas Fricke <mail@nicolasfricke.de>
2022-05-05 19:04:52 +0200

    Add support for a button to start picture taking
    The push button connected to pin 18 will trigger as our camera's
    shutter. Using the gpiozero library makes this particularly easy and
    using the pause() method from the signal library allows us to put the
    script in an implicit loop that just waits for these button presses and
    then jumps into action.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from camera import take_picture
 from image import load_resize_and_save
 from printer import print_image
+from gpiozero import Button
+from signal import pause
 
 
-if __name__ == '__main__':
+def take_and_print_a_picture():
     print("Taking a picture...")
     image_path = take_picture()
     print("Done, saved as " + image_path)
@@ -16,4 +18,17 @@ if __name__ == '__main__':
 
     print_image(resized_image_path, "/dev/serial0")
 
-    print("Done.")
+    print("Done. Wanna go again? Press the button...")
+
+
+if __name__ == '__main__':
+    print("The photo booth is ready!")
+    print("Press the button when you're, too...")
+
+    shutter_button = Button(18)
+    shutter_button.when_pressed = take_and_print_a_picture
+
+    # Using signal's pause method to put the script in a
+    # waiting loop handling button presses whenever they
+    # come:
+    pause()
